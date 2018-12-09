a5c159bb20cd6dbf352e97c61891ebaaa18032f6
Unknown <romancottinet@gmail.com>
2018-12-09 12:39:30 +0100

    Config.properties

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -3,16 +3,30 @@ package Controller;
 import Model.Level;
 import View.ConsoleOutput;
 import View.Menu;
+import sun.net.ProgressSource;
 
 import javax.swing.*;
+import java.util.ResourceBundle;
 
 public class Controller {
 
     private static Level lvl;
 
+
+
+
+
     public static void main(String[] args) {
+
+
+        ResourceBundle bundle = ResourceBundle.getBundle("Ressources.domaine.properties.config");
+
+        String level_load = bundle.getString("lvl_1");
+
         lvl = new Level();
-        lvl.loadLevel("src/Ressources/Map/map_01");
+
+
+        lvl.loadLevel(level_load);
 
         Menu menu = new Menu(lvl);
 
